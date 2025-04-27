b08cd6757abc647afa4ec1f6024b3ef0ef0bde9c
Nahoo14 <48842658+Nahoo14@users.noreply.github.com>
2025-04-27 15:54:29 -0700

    Add Watch module

---
+++ Time_Counter/time-counter-watch Watch App/time_counter_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_counter_watchApp.swift
+//  time-counter-watch Watch App
+//
+//  Created by Baby Tinishu on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct time_counter_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
